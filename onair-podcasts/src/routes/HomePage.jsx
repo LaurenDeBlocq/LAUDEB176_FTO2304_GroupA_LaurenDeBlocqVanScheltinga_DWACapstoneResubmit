b85e5b5b897dc99b268242476f74dd5cf8726fd8
Lauren de Blocq <lauren.deblocq@gmail.com>
2023-11-05 16:22:28 +0200

    Added Filter back

--- onair-podcasts/src/routes/HomePage.jsx
+++ onair-podcasts/src/routes/HomePage.jsx
@@ -1,16 +1,101 @@
 import React from "react";
+import Fuse from "fuse.js";
 import Card from "../components/Card";
 import { useGetPodcastsQuery } from "../services/podcasts";
 import CarouselPage from "../components/Carousel";
+import Filter from "../components/Filter";
 
 function HomePage() {
-  const { data, isLoading } = useGetPodcastsQuery();
+  const { data, isLoading, refetch } = useGetPodcastsQuery();
+  const [loadedShows, setLoadedShows] = React.useState([]);
+  const [searchTerm, setSearchTerm] = React.useState("");
+  const [filterType, setFilterType] = React.useState("Default");
+  React.useEffect(() => {
+    if (data) {
+      setLoadedShows(data);
+    }
+  }, [data]);
+
+  const filterShows = () => {
+    const fuse = new Fuse(data, {
+      includeScore: true,
+      keys: ["title"],
+    });
+    let filtered = searchTerm !== "" ? fuse.search(searchTerm) : data;
+
+    if (filterType === "Default" && searchTerm !== "") {
+      filtered.sort((a, b) => {
+        b.score - a.score;
+      });
+    }
+    if (searchTerm !== "") {
+      filtered = filtered.map((item) => item.item);
+    }
+    if (filterType === "A-Z") {
+      filtered.sort((a, b) => {
+        let nameA = a.title.toLowerCase();
+        let nameB = b.title.toLowerCase();
+
+        if (nameA < nameB) return -1;
+        if (nameA > nameB) return 1;
+        return 0;
+      });
+    }
+    if (filterType === "Z-A") {
+      filtered.sort((a, b) => {
+        let nameA = a.title.toLowerCase();
+        let nameB = b.title.toLowerCase();
+
+        if (nameA > nameB) return -1;
+        if (nameA < nameB) return 1;
+        return 0;
+      });
+    }
+    if (filterType === "Oldest") {
+      filtered.sort((a, b) => {
+        let dateA = new Date(a.updated);
+        let dateB = new Date(b.updated);
+
+        return dateA - dateB;
+      });
+    }
+    if (filterType === "Newest") {
+      filtered.sort((a, b) => {
+        let dateA = new Date(a.updated);
+        let dateB = new Date(b.updated);
+
+        return dateB - dateA;
+      });
+    }
+    setLoadedShows(filtered);
+  };
+
+  React.useEffect(() => {
+    if (searchTerm && filterType) {
+      filterShows();
+    }
+  }, [searchTerm, filterType]);
+
   if (isLoading) {
     return <div className="loading">Loading...</div>;
   }
-  const previewCards = data.map((show) => {
+
+  const handleFilterSearch = (event) => {
+    const filterSearch = setTimeout(() => {
+      console.log("wait");
+      setSearchTerm(event.target.value);
+    }, 300);
+    return () => clearTimeout(filterSearch);
+  };
+
+  const handleFilterSelect = (event) => {
+    const filter = event.target.value;
+    setFilterType(filter);
+  };
+  const previewCards = loadedShows.map((show) => {
     return <Card key={show.id} showData={show} />;
   });
+
   const carouselDetails = [
     data[Math.floor(Math.random() * data.length)],
     data[Math.floor(Math.random() * data.length)],
@@ -20,6 +105,10 @@ function HomePage() {
   return (
     <>
       <CarouselPage details={carouselDetails} />
+      <Filter
+        handleFilterSelect={handleFilterSelect}
+        handleFilterSearch={handleFilterSearch}
+      />
       {previewCards}
     </>
   );
